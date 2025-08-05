8a481e0489eb037a99172b485da9bf2f357487a5
wxzvd <forworknickworron@gmail.com>
2025-08-06 02:43:14 +0500

    Update: next.config configurated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+
+
+
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	env: {
+		SERVER_URL: process.env.SERVER_URL
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'avatars.yandex.net'
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
