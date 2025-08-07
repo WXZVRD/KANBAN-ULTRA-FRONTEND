a830fb3531c81bdcf44deb12ae8869b3b116446d
wxzvd <forworknickworron@gmail.com>
2025-08-07 14:52:06 +0500

    Add: login and register pages

---
+++ src/app/auth/login/login.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Вход',
+	description: ''
+}
+
+export default function LoginPage() {
+	return <h1>Login page</h1>
+}
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Регистрация',
+	description: ''
+}
+
+export default function RegisterPage() {
+	return <h1>Register page</h1>
+}
