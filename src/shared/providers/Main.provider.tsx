cf6500677952cf734ebb6b06b63049a40217671a
wxzvd <forworknickworron@gmail.com>
2025-08-07 16:48:24 +0500

    Add: ToastProvider implement

--- src/shared/providers/Main.provider.tsx
+++ src/shared/providers/Main.provider.tsx
@@ -3,11 +3,13 @@
 import { JSX, type PropsWithChildren } from 'react'
 
 import { TanstackQueryProvider, ThemeProvider } from '@/shared/providers'
+import { ToastProvider } from '@/shared/providers/Toast.provider'
 
 export function MainProvider({ children }: PropsWithChildren): JSX.Element {
 	return (
 		<TanstackQueryProvider>
 			<ThemeProvider attribute='class' defaultTheme='light'>
+				<ToastProvider />
 				{children}
 			</ThemeProvider>
 		</TanstackQueryProvider>
---
+++ src/shared/providers/Toast.provider.tsx
@@ -0,0 +1,5 @@
+import { Toaster } from '@/shared/components/ui'
+
+export function ToastProvider() {
+	return <Toaster position='bottom-right' duration={6000} />
+}
