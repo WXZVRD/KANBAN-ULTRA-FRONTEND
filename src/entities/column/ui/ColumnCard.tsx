2cd69759bd3ba25d5322e54126da7b00646b7c14
wxzvd <forworknickworron@gmail.com>
2025-08-21 17:07:14 +0500

    Add: rename column functuionality

--- src/entities/column/ui/ColumnCard.tsx
+++ src/entities/column/ui/ColumnCard.tsx
@@ -1,18 +1,69 @@
+import { useState } from 'react'
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/shared/components/ui'
 
 import { ColumnCardSettings } from '@/entities/column/ui/ColumnCardSettings'
+import { useRenameColumn } from '@/feautures/projectColumn/rename-column/model/useRenameColumn'
 
 interface ColumnCardProps {
+	columnId: string
 	title: string
 	projectId: string
 	children?: React.ReactNode
 }
 
-export const ColumnCard = ({ title, projectId, children }: ColumnCardProps) => {
+export const ColumnCard = ({
+	columnId,
+	title,
+	projectId,
+	children
+}: ColumnCardProps) => {
+	const { renameColumn, isNameChanging } = useRenameColumn()
+
+	const [isEditing, setIsEditing] = useState(false)
+	const [newTitle, setNewTitle] = useState<string>(title)
+
+	const handleBlur = () => {
+		setIsEditing(false)
+		if (newTitle.trim() && newTitle !== title) {
+			renameColumn({ columnId, title: newTitle })
+		} else {
+			renameColumn({ columnId, title: newTitle })
+			setNewTitle(title)
+		}
+	}
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			e.currentTarget.blur()
+		}
+		if (e.key === 'Escape') {
+			renameColumn({ columnId, title: newTitle })
+			setNewTitle(title)
+			setIsEditing(false)
+		}
+	}
+
 	return (
 		<Card className='w-80 flex-shrink-0'>
 			<CardHeader className='flex justify-between'>
-				<CardTitle>{title}</CardTitle>
+				{isEditing ? (
+					<input
+						className='w-full rounded border px-2 py-1 text-sm'
+						value={newTitle}
+						autoFocus
+						onChange={e => setNewTitle(e.target.value)}
+						onBlur={handleBlur}
+						onKeyDown={handleKeyDown}
+					/>
+				) : (
+					<CardTitle
+						className='cursor-pointer'
+						onClick={() => setIsEditing(true)}
+					>
+						{title}
+					</CardTitle>
+				)}
 				<div className=''>
 					<ColumnCardSettings title={title} projectId={projectId} />
 				</div>
--- src/feautures/projectColumn/components/ColumnView.tsx
+++ src/feautures/projectColumn/components/ColumnView.tsx
@@ -11,17 +11,20 @@ export function ColumnView({ columns }: IColumnView) {
 	const params = useParams<{ projectId: string }>()
 
 	return (
-		<div className="w-full overflow-x-auto">
-			<div className="flex min-h-[72vh] gap-4 p-4">
+		<div className='w-full overflow-x-auto'>
+			<div className='flex min-h-[72vh] gap-4'>
 				{columns.map((col, idx) => (
 					<ColumnCard
-						key={idx}
+						key={col.id}
+						columnId={col.id}
 						title={col.title}
 						projectId={params.projectId}
 					/>
 				))}
 
-				<AddColumnButton columnsLength={columns.length++} />
+				{columns.length < 5 && (
+					<AddColumnButton columnsLength={columns.length++} />
+				)}
 			</div>
 		</div>
 	)
