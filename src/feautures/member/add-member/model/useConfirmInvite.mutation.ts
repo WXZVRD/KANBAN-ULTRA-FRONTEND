f84d8734a69901788713c8cc0e7afb96c2f5dae2
wxzvd <forworknickworron@gmail.com>
2025-09-04 17:58:34 +0500

    Clean up: cleaning unnecessary code

--- src/feautures/member/add-member/model/useConfirmInvite.mutation.ts
+++ src/feautures/member/add-member/model/useConfirmInvite.mutation.ts
@@ -1,9 +1,11 @@
+'use client'
+
 import { toast } from 'sonner'
 
 import { APP_ROUTES } from '@/shared/consts'
 import { toastMessageHandler } from '@/shared/utils'
 
-import { confirmInvite, IConfirmInviteDTO } from '@/feautures/member/add-member/api/confirmInvite.api'
+import { confirmInvite, IConfirmInviteDTO } from '@/feautures/member'
 import { useMutation } from '@tanstack/react-query'
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
 import { useRouter } from 'next/navigation'
